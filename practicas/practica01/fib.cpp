a13ef1d9cdf2eca428d8547160c8c9dec78f3ab6
mdrorum <mdrorum@gmail.com>
2013-10-21 11:45:45 +0200

    Make compila y documenta

--- practicas/practica01/fib.cpp
+++ practicas/practica01/fib.cpp
@@ -1,21 +1,36 @@
-/** * fib.cpp
-    * Código que muestra la secuencia de Finonacii de la longitud dada *
-    * By Óscar Romero *
-*/
+/** \file fib.cpp
+ * Generación de la serie de Fibonacii, donde cada término es la suma de los dos anteriores.
+ * 
+ * El usuario debe introducir la pareja de números iniciales y el número de iteraciones.
+ *
+ * <a href="http://es.wikipedia.org/wiki/Sucesi%C3%B3n_de_Fibonacci">Serie de Fibonacii en Wikipedia</a> 
+ * 
+ * \date 19 de Octubre de 2013
+
+ */
+
+// El archivo de cabecera "iostream" permite usar cout y cin.
 #include <iostream>
 
-using namespace std;
-int a, b;     //Los números que inician la serie
-int limite;   //Número limite de iteraciones
-int iter = 0; //Número de iteraciones
-int suma;     //Suma de los números de la serie
+//! Primer valor de la serie
+int a;
+//! Segundo valor de la serie
+int b;
+//! Número de términos de la serie a mostrar.
+int limite;
+//! Iteración actual.
+int iter = 0;
+/// Suma de los dos valores anteriores.
+int suma;
 
+//! La función principal solicita los dos números iniciales de la serie y el número de iteraciones
 int main(){
-	std::cout << "Introduzca los numeros iniciales de la serie, separados por un espacio: " << std::endl;
-	std:cin >> a >> b; 
-	std::cout << endl;
 
-	std::cout << "Introduzca el numero de iteraciones en la serie: " << std::endl;
+	std::cout << "Introduzca la pareja de números iniciales de la serie, separados por un espacio: " << std::endl;
+	std::cin >> a >> b; 
+	std::cout << std::endl;
+
+	std::cout << "Introduzca el número de iteraciones en la serie: " << std::endl;
 	std::cin >> limite;
 	std::cout << a << " " << b << " ";
 
@@ -26,7 +41,7 @@ int main(){
 		a = b;
 		b = suma;
 	}
-	std::cout << endl;
+	std::cout << std::endl;
 	return 0;
 }
 
