c5c572de5d8b79525799b59dcd3e9fdd1f347369
mucgs <mucgs@MUCG16.u-tad.es>
2013-12-09 19:47:40 +0100

    a por comunicaciones

--- robots/main.cpp
+++ robots/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include "bots.h"
-#include <set>
 #define _LADO 10
 
-void pinta_escenario()
+void pinta_escenario( bots mis_bots, int paso)
 {
-	//Llena la pila ordenada de robots
-	
+	std::cout << "Paso : " << paso << std::endl;
+
 	for ( int col = 0; col < _LADO + 2; col++ ) // Primera fila de X
         	std::cout << 'X'; 
 	std::cout << std::endl;
@@ -14,53 +13,60 @@ void pinta_escenario()
         {
 		std::cout << 'X'; // Primera X de la fila
 		for ( int col = 0; col < _LADO; col++ ) // Espacios en blanco
-                        std::cout << ' '; 
-		std::cout << 'X'; //! Espacios de 1 a altura - fila
+		{
+			bot * pos_bot = mis_bots.find_at( { col, fila} );
+			//std::cout << pos_bot->get_team();
+			
+			if ( pos_bot == nullptr )
+                        	std::cout << ' '; 
+			else
+				std::cout << pos_bot->get_team();
+				
+		}
+		std::cout << 'X'; //! Espacios de 1a altura - fila
                 std::cout << std::endl;
         }
 	for ( int col = 0; col < _LADO + 2; col++ ) // Primera fila de X
                         std::cout << 'X'; 
 	std::cout << std::endl;
-
 }
 
 
 
 int main (int argc, char* argv[])
 {
-	
-	int myints[] = {9,14,17,4,52};
-  std::set<int> myset (myints,myints+5);
-
-
-  std::set<int>::iterator it;
-
-  std::cout << "myset contains:";
-  for ( it=myset.begin() ; it != myset.end(); it++ )
-    std::cout << " " << *it;
-
-  std::cout << std::endl;
+	int paso = 0;
 
 	bots mi_bots = bots(_LADO, _LADO); // Tamaño del campo 10x10, usa la pila
 	// bots *mi_bots = new bots(10,10) // al monticulo
 
-	mi_bots.generate(1,4); // Número de equipos y número de miembros por equipo, usa la pila
+	mi_bots.generate(4,4); // Número de equipos y número de miembros por equipo, usa la pila
 	// mi_bots->generate(3,10); // al monticulo
 
-	
+	std::cout << "\x1B[2J\x1B[H";
+
 	while (true) 
 	{
 		mi_bots.step();
-		mi_bots.for_each_bot([] (const bot & b) {
+		paso++;
+
+		mi_bots.for_each_bot([&] ( bot & b ) {
 			//std::cout << b.get_team() << "\t"; // std::endl;
 			//std::cout << b.get_x() << "\t";
 			//std::cout << b.get_y() << "\t";
 			//std::cout << b.get_energy() << "\t";
-			//std::cout << std::endl;    
+			//std::cout << std::endl;
+			
+			//mi_bots.can_move( b, bot::W);
+			if ( b.get_team() == 0) 
+			{
+				b.try_to_do(bot::W);
+			}
 		});
-		
+
+				
 		//pinta_escenario(mis_bots_ord);
-		pinta_escenario();
+		pinta_escenario(mi_bots, paso);
 
 		while ( std::cin.get() != ' ' );
 		std::cout << "\x1B[2J\x1B[H";
