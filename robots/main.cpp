e6b211df899601652fe34be25e7c8d21f37e167b
mucgs <mucgs@MUCG16.u-tad.es>
2013-12-11 18:10:24 +0100

    Miercoles

--- robots/main.cpp
+++ robots/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include "bots.h"
-#define _LADO 10
+#include <boost/asio.hpp>
+#include <boost/thread/thread.hpp>
+
+const int _LADO = 10;
+
+
+using boost::asio::ip::tcp;
+
+void send(tcp::socket &socket, const std::string & str) 
+{
+    boost::asio::write(socket, boost::asio::buffer(str + "\n"), boost::asio::transfer_all());
+}
 
 void pinta_escenario( bots mis_bots, int paso)
 {
@@ -35,17 +46,39 @@ void pinta_escenario( bots mis_bots, int paso)
 
 int main (int argc, char* argv[])
 {
+
+	std::cout << "\x1B[2J\x1B[H"; // Despeja la pantalla del terminal
+	std::cout << "A jugar!!"
+	return(0);
+	boost::asio::io_service io_service; // Inicia comunicaciones
+
+	tcp::resolver resolver(io_service); // Resuelve la comunicación
+	auto endpoint_iterator = resolver.resolve({ argv[1], argv[2] }); // Iterador hacia puerto y dirección
+
+	std::shared_ptr<tcp::socket> socket(new tcp::socket(io_service)); // Puntero compartido hacia el socket
+	boost::asio::connect(*socket, endpoint_iterator); // Conexión
+
 	int paso = 0;
 
+	// Variables del hilo
+	boost::mutex state_mutex;
+	bool gameover = false;
+	bool connected = false;
+	bot::team_id id = 6666;
+	bot::field_size field_width;
+	bot::field_size field_height;
+	int win_width = 500;
+	int win_height = 500;
+        
 	bots mi_bots = bots(_LADO, _LADO); // Tamaño del campo 10x10, usa la pila
 	// bots *mi_bots = new bots(10,10) // al monticulo
 
 	mi_bots.generate(4,4); // Número de equipos y número de miembros por equipo, usa la pila
 	// mi_bots->generate(3,10); // al monticulo
+	
 
-	std::cout << "\x1B[2J\x1B[H";
 
-	while (true) 
+	while (!gameover) 
 	{
 		mi_bots.step();
 		paso++;
@@ -58,12 +91,18 @@ int main (int argc, char* argv[])
 			//std::cout << std::endl;
 			
 			//mi_bots.can_move( b, bot::W);
-			if ( b.get_team() == 0) 
-			{
-				b.try_to_do(bot::W);
-			}
+			
+			//if ( b.get_team() == 0) 
+			//{
+			//	b.try_to_do(bot::W);
+			//}
+			
+			std::stringstream stream;
+            //stream << "move " << b.get_x() << " " << b.get_y() << " " << b.get_next_direction();
+            stream << "gameover";
+            send(*socket, stream.str());
+			
 		});
-
 				
 		//pinta_escenario(mis_bots_ord);
 		pinta_escenario(mi_bots, paso);
